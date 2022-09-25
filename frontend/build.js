ba85f7357a136fd02841a80dc0060c2b7bde4e53
Jake Weilhammer <jake.weilhammer@gmail.com>
2022-09-25 16:04:44 -0400

    Use default cache for songs to prevent bad data from getting cached

--- frontend/build.js
+++ frontend/build.js
@@ -8,16 +8,21 @@ const config = defaults.__get__('config');
 for (const plugin of config.plugins) {
     if (plugin.config && Object.keys(plugin.config).includes("swDest")) {
         plugin.config.runtimeCaching = [{
-            urlPattern: "/songs.json",
-            handler: 'CacheFirst',
-            options: {
-              cacheName: 'song-data',
-              // Expire songs after 1 day
-              // Client will only hit Network for songs once per day
-              expiration: {
-                maxAgeSeconds: 30
-              },
-            },
+            urlPattern: '/songs.json',
+            // Use default handler to serve from cache when available and update in background
+            // Just in case bad responses get out there, don't want to cache them for too long
+            handler: 'StaleWhileRevalidate',
+
+            // For use with the 'CacheFirst' strategy
+            // TODO: Enable this when can guarentee 404 on React route
+            // options: {
+            //   cacheName: 'song-data',
+            //   // Expire songs after 1 day
+            //   // Client will only hit Network for songs once per day
+            //   expiration: {
+            //     maxAgeSeconds: 24 * 60 * 60,
+            //   },
+            // },
           }
         ]
     }
